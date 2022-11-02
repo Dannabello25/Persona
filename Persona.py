ae0abcf47a1a4eaf22222d0e9fb682dcfa5fe4ab
Dannabello <dmbellom@correo.udistrital.edu.co>
2022-11-02 17:22:25 -0500

    v.0.0.1

---
+++ Persona.py
@@ -0,0 +1,38 @@
+class Persona:
+    def __init__(self):
+        self.nombre=""
+        self.edad=int
+        self.genero=""
+        self.volumen=""
+        self.bigote=bool
+
+    def gritar(self):
+        pass
+    def bailar(self):
+        pass
+    def saltar(self):
+        pass
+
+sujeto_a=Persona()
+sujeto_a.nombre="Camilo"
+sujeto_a.edad=20
+sujeto_a.genero="pansexual"
+sujeto_a.volumen="alto"
+sujeto_a.bigote=False
+
+sujeto_b=Persona()
+sujeto_b.nombre="Cristian"
+sujeto_b.edad=56
+sujeto_b.genero="masculino"
+sujeto_b.volumen="medio"
+sujeto_b.bigote=False
+
+sujeto_c=Persona()
+sujeto_c.nombre="Lina"
+sujeto_c.edad=19
+sujeto_c.genero="femenino"
+sujeto_c.volumen="medio"
+sujeto_c.bigote=False
+
+
+    
